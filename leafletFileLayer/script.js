79484540949bffe3604a0094cc69011424bda2ba
Mingfei Yin <mingfei@Mingfeis-MacBook-Pro.local>
2017-10-30 20:31:02 -0500

    add comments to js

--- leafletFileLayer/script.js
+++ leafletFileLayer/script.js
@@ -1,7 +1,7 @@
 (function (window) {
     'use strict';
 
-    function initMap() {
+    function initMap() { //load the map
         var L = window.L;
         var osm = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: 'Map data &copy; 2013 OpenStreetMap contributors'
@@ -9,16 +9,16 @@
         var map = L.map('map', {
             center: [0, 0],
             zoom: 2
-        }).addLayer(osm);
-        var style = {
-            color: 'red',
-            opacity: 1.0,
-            fillOpacity: 1.0,
-            weight: 2,
+        }).addLayer(osm); //runs and configures Leaflet.FileLayer
+        var style = {  //sets the styles for the control and the loaded gps traces
+            color: 'crimson',
+            opacity: 0.8,
+            fillOpacity: 0.8,
+            weight: 0.1,
             clickable: false
         };
-        L.Control.FileLayerLoad.LABEL = '<img class="icon" src="../docs/folder.svg" alt="file icon"/>';
-        L.Control.fileLayerLoad({
+        L.Control.FileLayerLoad.LABEL = '<img class="icon" src="folder.svg" alt="file icon"/>'; // declare the icon to initiate the file opening process 
+        L.Control.fileLayerLoad({ 
             fitBounds: true,
             layerOptions: {
                 style: style,
